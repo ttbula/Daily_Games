5d17cce9ab888c311c610bb08b96a9faacad5c91
Timothy Cebula <timothycebula@gmail.com>
2023-11-30 18:32:36 -0600

    skeleton of project is created

---
+++ Frontend/app.js
@@ -0,0 +1,9 @@
+/*
+Im gonna need 
+1. Logic for selecting the buttons
+    As in: a button either tim, demetri, or griffin is selected
+    Put an event handler in there to listen to which button is selected
+    When you select a button, all data needs to go to that unique person
+    When you enter times into text fields that data needs to get pushed to that unique person
+    All that data needs to get stored to an array somewhere in the backend
+*/
